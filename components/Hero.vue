c081aaaebe7c083baca5e0c66a0951002748f895
daletom <tom@imgix.com>
2021-08-12 10:46:45 -0700

    testing slug dynamic

---
+++ components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center">
+      <div
+      class="py-8 px-8 mx-auto bg-yellow-100 rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 md:py-4 md:flex md:items-center md:space-y-0 md:space-x-6"
+      v-for="article in data" 
+      :key="article.id"
+      >
+        <ix-img
+        class="lg:w-1/2 md:w-1/3 w-full"
+        :src="article.metadata.avatar.imgix_url"
+        loading="lazy"
+        :imgixParams="{ fit: 'crop', ar: '1.8:1', crop: 'faces,edges' }"
+        sizes="(min-width: 1024px) calc(60vw - 192px), 75vw"
+        />
+        <div class="text-center space-y-2">
+          <div class="space-y-0.5">
+            <h1 class="p-4 text-4xl">{{article.title}}</h1>
+          <p class="blog-description inline text-xl" v-html="article.content"></p> 
+          </div>
+        </div>
+      </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: ['data']
+}
+</script>
---
+++ pages/lokis/_id.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="p-4">
+    <Heroes v-bind:data="getObjects.objects" />
+  </div>
+</template>
+
+<script>
+import getObjects from '../../apollo/queries/oneHero.gql'
+
+export default {
+    apollo: {
+    getObjects: {
+      prefetch: true,
+      query: getObjects,
+      variables: {
+          slug: route.params.id
+      }
+    }
+  },
+    /*async asyncData({app, route}){
+            const {data} = await app.apolloProvider.defaultClient.query({
+            query: getObjects,
+            variables: {
+                slug: route.params.slug
+            }
+            });
+            return data
+        
+    },
+    mounted() {
+    console.log(this.myRoute);
+  },
+  computed: {
+    myRoute() {
+      return this.$route.params.slug;
+    },
+  },*/
+}
+</script>
